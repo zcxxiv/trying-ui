61d6cda1eb18f1012855ca1868943203405eebe7
Ziyuan Chen <zcxxiv@gmail.com>
2022-02-10 06:09:49 -0500

    Initial Commit

---
+++ Shared/TryingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryingApp.swift
+//  Shared
+//
+//  Created by ZC on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TryingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
